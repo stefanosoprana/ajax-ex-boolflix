dbdfa44156189c4a68a727abfca6e17f5cd287bf
stefanosoprana <stefano.soprana@icloud.com>
2018-12-18 21:01:35 +0100

    search tv-series

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $('button').click(function(){
     var input = $('input').val()
 
+    $('.movie').html('')
+
     $.ajax({
       url: 'https://api.themoviedb.org/3/search/movie',
       data: {
@@ -12,26 +14,63 @@ $(document).ready(function(){
       },
       method: 'GET',
       success: function(data){
-        $('.movie').html('')
         var raccoltaFilm = data.results;
         console.log(raccoltaFilm);
         for (var i = 0; i < raccoltaFilm.length; i++) {
+
           var film = raccoltaFilm[i];
 
-          var arrotondamento = Math.ceil(film.vote_average / 2);
-
-          var lingua = film.original_language;
-
-          console.log(lingua);
-
-          var source   = $('#movie-template').html()
+          var source = $('#movie-template').html()
           var template = Handlebars.compile(source);
 
           var context = {
             title: film.title,
             original_title: film.original_title,
             language: flag(film.original_language),
-            rating: starRate(arrotondamento),
+            rating: starRate(film.vote_average),
+          };
+          var html = template(context);
+
+          $('.movie').append(html)
+
+          // var titolo = film.title;
+          // console.log(titolo);
+          // var titolo_originale = film.original_title;
+          // console.log(titolo_originale);
+          //
+          // doubleTitle(titolo, titolo_originale);
+
+        }
+      },
+      error: function(){
+        alert('si è verificato un errore')
+      }
+    });
+
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/tv',
+      data: {
+        api_key: '34da49db03cff40a92cb2b8f356c68fe',
+        language: 'it',
+        query: input
+      },
+      method: 'GET',
+      success: function(data){
+        var raccoltaTv = data.results;
+        console.log(raccoltaTv);
+        for (var i = 0; i < raccoltaTv.length; i++) {
+          var serieTv = raccoltaTv[i];
+
+          // doubleTitle(serieTv.name, serieTv.original_name);
+
+          var source = $('#movie-template').html()
+          var template = Handlebars.compile(source);
+
+          var context = {
+            title: serieTv.name,
+            original_title: serieTv.original_name,
+            language: flag(serieTv.original_language),
+            rating: starRate(serieTv.vote_average),
           };
           var html = template(context);
 
@@ -42,24 +81,29 @@ $(document).ready(function(){
         alert('si è verificato un errore')
       }
     });
+
+    $('input').val('');
+
   });
+
+  // function doubleTitle(titolo,titolo_originale) {
+  //   if (titolo == titolo_originale) {
+  //     $('.titolo_originale').addClass('active')
+  //   };
+  // }
 
   function starRate(counter){
 
     var star = '';
+    var numeroIntero = Math.ceil(counter / 2);
+    var diff = 5 - numeroIntero;
 
-    var diff = 5 - counter;
-
-    for (var i = 0; i < counter; i++) {
-
+    for (var i = 0; i < numeroIntero; i++) {
       star += '<i class="fas fa-star"></i>';
-
     };
 
     for (var i = 0; i < diff; i++) {
-
       star += '<i class="far fa-star"></i>';
-
     };
 
     return star
@@ -90,6 +134,5 @@ $(document).ready(function(){
     return language
 
   };
-
 
 });
